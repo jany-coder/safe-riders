edf010853fc643e967a24134dcb07fba424b1085
jany-coder <jany_ete@yahoo.com>
2021-03-19 18:22:26 +0600

    login btn

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Hello</h1>
+
+      <Router>
+          <Switch>
+              <Route path="/login">
+                  <Login>Login</Login>
+              </Route>
+          </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA00MCbJs_DwaQs-A6i-pNaHh2xDbAFmBo",
+    authDomain: "safe-riders-a2d4d.firebaseapp.com",
+    projectId: "safe-riders-a2d4d",
+    storageBucket: "safe-riders-a2d4d.appspot.com",
+    messagingSenderId: "904932999643",
+    appId: "1:904932999643:web:4eff5b4b04a40e6178a7fe"
+  };
+
+  export default firebaseConfig;
